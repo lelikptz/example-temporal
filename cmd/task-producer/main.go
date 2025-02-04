dc90598eae2e783665e2299aaaf6cafcb71e48d3
lelikptz <alexey@orlov.tech>
2025-02-04 20:08:04 +0100

    added cancel signal producer

--- cmd/task-producer/main.go
+++ cmd/task-producer/main.go
@@ -4,10 +4,15 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"math/rand"
 	"sync"
 	"time"
 
+	"go.temporal.io/api/common/v1"
+	"go.temporal.io/api/workflowservice/v1"
 	"go.temporal.io/sdk/client"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
@@ -33,10 +38,12 @@ type Order struct {
 
 func runTask(temporalClient client.Client, wg *sync.WaitGroup) {
 	defer wg.Done()
+	ctx := context.Background()
+	ID := fmt.Sprintf("%s", time.Now().Format(time.DateTime))
 	_, err := temporalClient.ExecuteWorkflow(
-		context.Background(),
+		ctx,
 		client.StartWorkflowOptions{
-			ID:        fmt.Sprintf("%s", time.Now().Format(time.DateTime)),
+			ID:        ID,
 			TaskQueue: "order-queue",
 		},
 		"OrderWorkflow",
@@ -48,4 +55,29 @@ func runTask(temporalClient client.Client, wg *sync.WaitGroup) {
 		log.Printf("Unable to execute workflow %s", err)
 		return
 	}
+
+	time.Sleep(time.Duration(rand.Intn(60)) * time.Second)
+
+	conn, err := grpc.NewClient(client.DefaultHostPort, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalln("Unable to create client", err)
+	}
+	defer conn.Close()
+
+	grpcClient := workflowservice.NewWorkflowServiceClient(conn)
+	req := &workflowservice.DescribeWorkflowExecutionRequest{
+		Namespace: "default",
+		Execution: &common.WorkflowExecution{
+			WorkflowId: ID,
+		},
+	}
+	resp, _ := grpcClient.DescribeWorkflowExecution(ctx, req)
+
+	_ = temporalClient.SignalWorkflow(
+		ctx,
+		resp.WorkflowExecutionInfo.GetExecution().GetWorkflowId(),
+		resp.WorkflowExecutionInfo.GetExecution().GetRunId(),
+		"cancel-order-signal",
+		nil,
+	)
 }
