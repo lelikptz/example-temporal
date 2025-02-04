fb04ce5587fb2171d52dc93be6d38984b8ec9bd5
lelikptz <alexey@orlov.tech>
2025-02-04 19:19:38 +0100

    added cancel signal listener

--- cmd/order-management-system/main.go
+++ cmd/order-management-system/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"log"
-	"time"
 
 	"go.temporal.io/sdk/client"
 	"go.temporal.io/sdk/worker"
@@ -30,41 +29,4 @@ func main() {
 	if err != nil {
 		log.Fatalln("Unable to start Worker", err)
 	}
-}
-
-func createOrder(ctx workflow.Context) error {
-	return workflow.ExecuteActivity(workflow.WithActivityOptions(ctx, workflow.ActivityOptions{
-		TaskQueue:              "create-order",
-		ScheduleToCloseTimeout: time.Minute,
-	}), "CreateOrder").Get(ctx, nil)
-}
-
-func sendOrder(ctx workflow.Context) (bool, error) {
-	var sendOrderResult = false
-	err := workflow.ExecuteActivity(workflow.WithActivityOptions(ctx, workflow.ActivityOptions{
-		TaskQueue:              "send-order",
-		ScheduleToCloseTimeout: time.Minute,
-	}), "SendOrder", workflow.GetInfo(ctx).WorkflowExecution.ID).Get(ctx, &sendOrderResult)
-
-	return sendOrderResult, err
-}
-
-func sendCancel(ctx workflow.Context) error {
-	return workflow.ExecuteActivity(workflow.WithActivityOptions(ctx, workflow.ActivityOptions{
-		TaskQueue:              "cancel-order",
-		ScheduleToCloseTimeout: time.Minute,
-	}), "CancelOrder").Get(ctx, nil)
-}
-
-func statusPolling(ctx workflow.Context) (string, error) {
-	var status string
-	err := workflow.ExecuteActivity(workflow.WithActivityOptions(ctx, workflow.ActivityOptions{
-		TaskQueue:              "status-polling",
-		ScheduleToCloseTimeout: time.Minute,
-	}), "StatusPoll").Get(ctx, &status)
-	if err != nil {
-		return "", err
-	}
-
-	return status, nil
 }
--- cmd/order-management-system/order_processor.go
+++ cmd/order-management-system/order_processor.go
@@ -3,6 +3,7 @@ package main
 import (
 	"time"
 
+	"go.temporal.io/sdk/log"
 	"go.temporal.io/sdk/workflow"
 )
 
@@ -11,79 +12,126 @@ const GetOrderStatusQuery = "get_order_status"
 type OrderStatus string
 
 const (
-	OrderStatusNew        OrderStatus = "New"
-	OrderStatusCreated    OrderStatus = "Created"
-	OrderStatusConfirmed  OrderStatus = "Confirmed"
-	OrderStatusSend       OrderStatus = "Send"
-	OrderStatusFailedSend OrderStatus = "FailedSend"
-	OrderStatusFinished   OrderStatus = "Finished"
-	OrderStatusCanceled   OrderStatus = "Canceled"
+	OrderStatusNew      OrderStatus = "New"
+	OrderStatusFinished OrderStatus = "Finished"
+	OrderStatusCanceled OrderStatus = "Canceled"
 )
 
 type Order struct {
 	ReservationTime time.Time
 }
 
-type OrderProcessor struct{}
+type OrderProcessor struct {
+	logger log.Logger
+}
 
 func NewOrderProcessor() *OrderProcessor {
 	return &OrderProcessor{}
 }
 
 func (o *OrderProcessor) Process(ctx workflow.Context, order Order) error {
-	logger := workflow.GetLogger(ctx)
+	o.logger = workflow.GetLogger(ctx)
+	o.logger.Info("ORDER PROCESS STARTED")
 
 	var orderStatus = OrderStatusNew
-	logger.Info("ORDER PROCESS STARTED")
-
 	_ = workflow.SetQueryHandler(ctx, GetOrderStatusQuery, func() (OrderStatus, error) {
-		logger.Info("GetOrderStatusQuery")
-
 		return orderStatus, nil
 	})
 
-	err := createOrder(ctx)
-	logger.Info("ORDER CREATED")
+	wg := workflow.NewWaitGroup(ctx)
+	workflow.Go(ctx, func(ctx workflow.Context) {
+		selector := workflow.NewSelector(ctx)
+		selector.AddReceive(workflow.GetSignalChannel(ctx, "cancel-order-signal"), func(c workflow.ReceiveChannel, more bool) {
+			c.Receive(ctx, nil)
+			orderStatus = OrderStatusCanceled
+			wg.Add(1)
+			_ = o.sendCancel(ctx)
+			o.logger.Info("SEND CANCEL FINISHED")
+			wg.Done()
+			return
+		})
+		selector.Select(ctx)
+	})
+	defer wg.Wait(ctx)
+
+	if orderStatus == OrderStatusCanceled {
+		return nil
+	}
+	err := o.createOrder(ctx)
 	if err != nil {
-		logger.Error("Create order failed", "error", err)
 		return err
 	}
 
-	err = workflow.Sleep(ctx, order.ReservationTime.Sub(workflow.Now(ctx))-10*time.Second)
+	if orderStatus == OrderStatusCanceled {
+		return nil
+	}
+	_ = workflow.Sleep(ctx, order.ReservationTime.Sub(workflow.Now(ctx))-10*time.Second)
+
+	if orderStatus == OrderStatusCanceled {
+		return nil
+	}
+	isSuccess, err := o.sendOrder(ctx)
 	if err != nil {
-		logger.Error("Sleep failed", "error", err)
 		return err
 	}
-
-	orderStatus = OrderStatusCreated
-	isSuccess, err := sendOrder(ctx)
-	logger.Info("ORDER SEND")
-	if err != nil {
-		logger.Error("Send order failed", "error", err)
-		return err
+	if orderStatus == OrderStatusCanceled {
+		return nil
 	}
-	if isSuccess {
-		orderStatus = OrderStatusSend
-		for orderStatus != OrderStatusFinished {
-			newStatus, err := statusPolling(ctx)
-			logger.Info("ORDER STATUS POLLING", "newStatus", newStatus)
-			if err != nil {
-				return err
-			}
-			orderStatus = OrderStatus(newStatus)
-		}
-
-	} else {
-		orderStatus = OrderStatusFailedSend
-		err = sendCancel(ctx)
+	if !isSuccess {
+		err = o.sendCancel(ctx)
 		if err != nil {
-			logger.Error("Send cancel failed", "error", err)
 			return err
 		}
-		orderStatus = OrderStatusCanceled
-		logger.Info("ORDER CANCELED")
+
+		return nil
 	}
 
-	logger.Info("ORDER PROCESS FINISHED")
+	for orderStatus != OrderStatusFinished && orderStatus != OrderStatusCanceled {
+		newStatus, err := o.statusPolling(ctx)
+		if err != nil {
+			return err
+		}
+		orderStatus = OrderStatus(newStatus)
+	}
+
+	o.logger.Info("ORDER PROCESS FINISHED")
 	return nil
+}
+
+func (o *OrderProcessor) createOrder(ctx workflow.Context) error {
+	o.logger.Info("ORDER CREATED")
+	return workflow.ExecuteActivity(workflow.WithActivityOptions(ctx, workflow.ActivityOptions{
+		TaskQueue:              "create-order",
+		ScheduleToCloseTimeout: time.Minute,
+	}), "CreateOrder").Get(ctx, nil)
+}
+
+func (o *OrderProcessor) sendOrder(ctx workflow.Context) (bool, error) {
+	var sendOrderResult = false
+	err := workflow.ExecuteActivity(workflow.WithActivityOptions(ctx, workflow.ActivityOptions{
+		TaskQueue:              "send-order",
+		ScheduleToCloseTimeout: time.Minute,
+	}), "SendOrder", workflow.GetInfo(ctx).WorkflowExecution.ID).Get(ctx, &sendOrderResult)
+
+	return sendOrderResult, err
+}
+
+func (o *OrderProcessor) sendCancel(ctx workflow.Context) error {
+	return workflow.ExecuteActivity(workflow.WithActivityOptions(ctx, workflow.ActivityOptions{
+		TaskQueue:              "cancel-order",
+		ScheduleToCloseTimeout: time.Minute,
+	}), "CancelOrder").Get(ctx, nil)
+}
+
+func (o *OrderProcessor) statusPolling(ctx workflow.Context) (string, error) {
+	var status string
+	err := workflow.ExecuteActivity(workflow.WithActivityOptions(ctx, workflow.ActivityOptions{
+		TaskQueue:              "status-polling",
+		ScheduleToCloseTimeout: time.Minute,
+	}), "StatusPoll").Get(ctx, &status)
+	if err != nil {
+		return "", err
+	}
+
+	return status, nil
 }
